663b6821195d4beefaf6ede9830764f2d2483b43
Jenna Howieson <jenna@jennahowieson.co.uk>
2022-11-28 16:34:07 +0000

    to-do list React Lab

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+  const [items, setItems] = useState([
+    { name: "Buy Shopping", priority: "high" },
+    { name: "Clean Bathroom", priority: "low" },
+    { name: "Car's MOT", priority: "high" },
+  ])
+
+  const [newItem, setNewItem] = useState("");
+  const [newPriority, setPriority] = useState("")
+
+  const itemNodes = items.map((item, index) => {
+    return (
+      <li key={index} className={item.priority === "high" ? "high" : "low"}>
+        <span>{item.name}</span>
+      </li>
+    )
+  })
+
+  const saveNewItem = (event) => {
+    event.preventDefault()
+    const newItemsArray = [...items];
+    newItemsArray.push({ name: newItem, priority: newPriority })
+    setItems(newItemsArray)
+    setNewItem("")
+  }
+
+  const handleInputChange = (event) => {
+    setNewItem(event.target.value);
+  }
+
+  const handleRadio =(event) => {
+    setPriority(event.target.value)
+  }
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To Do's</h1>
+
+
+      <form onSubmit={saveNewItem}>
+        <label htmlFor='new-item'> </label>
+        <input type="text"
+          id="new-item"
+          value={newItem}
+          onChange={handleInputChange}
+          placeholder="What do you want to do?">
+        </input>
+
+        <div onChange={handleRadio}>
+          <input type="radio" name="priority" value="high" />High
+          <input type="radio" name="priority" value="low" /> Low
+        </div>
+
+        <input type="submit" value="Save new item"></input>
+
+      </form>
+
+      <h2>{itemNodes}</h2>
+
     </div>
+
   );
 }
 
